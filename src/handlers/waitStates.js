8faeb53e3f5d128eba644dd8951b37cf0a70d810
Jason Yoo <jasonyoo0116@gmail.com>
2018-06-13 23:59:23 -0700

    more state management work

--- src/handlers/waitStates.js
+++ src/handlers/waitStates.js
@@ -8,16 +8,105 @@ const cache = require('../helpers/cache.js');
 const com = require('../helpers/communication.js');
 const db = require('../helpers/db.js');
 
+// Receive message or postback calls send function
+// Treat default state specially?
+
+const SEQUENCES = {
+  help: {
+    permittedStates: "All",
+    check: checkHelp, // User asks for help and passes check - State is default so eval send function
+    send: sendHelp, // Bot sends user help and doesn't expect a reply
+    newState: "Default",
+    afterWait: null,
+    nextActions: null
+  },
+  remind: {
+    check: checkRemind, // Poll update = State is default so eval send function
+    send: sendRemind, // Bot sends user reminder and expects a reply
+    newState: "ReceivingReminder", // While waiting, user state is ReceivingReminder
+    afterWait: analyzeRemind, // Bot saves 'Done' or 'NotDone' status and eval checks for nextActions - State not default so don't eval current send
+    nextActions: [ "options", "notDoneReason" ] // Upon being selected, eval nextAction's send function
+  },
+  options: {
+    check: checkOptions,
+    send: sendOptions, // Bot tells user to choose between virtues and hindrances
+    newState: "SelectingOptions", // While waiting, user state is SelectingOptions
+    afterWait: analyzeOptions, // Bot saves 'choice' and eval checks for nextActions - State not default so don't eval current send
+    nextActions: [ "virtues", "hindrances" ] // Upon being selected, eval nextAction's send function
+  },
+  virtues: {
+    check: checkVirtues,
+    send: sendVirtues, // Bot tells user to choose from virtue list
+    newState: "SelectingVirtues", // While waiting, user state is SelectingVirtues
+    afterWait: analyzeVirtues, // Bot saves 'choice' and eval checks for nextActions - State not default so don't eval current send
+    nextActions: [ "nextPlan" ] // Upon being selected, eval nextAction's send function
+  }
+  nextPlan: {
+    check: checkNextPlan,
+    send: sendNextPlan, // Bot tells user to write plan
+    newState: "CreatingPlan", // While waiting, user state is CreatingPlan
+    afterWait: analyzeNextPlan, // Bot saves 'entry' and eval checks for nextActions - State not default so don't eval current send
+    nextAction: null // Upon not being selected, revert state to "Default"
+  }
+}
+
+const ACTIONS = {
+  help: {
+    noNextAction: 'yes', // Trigger respondHandler
+    entryStates: 'all',
+    check: checkHelp, // Payload is "HELP" <ASSUMES STATE IS CORRECT>
+    respondHandler: respondHelp, // Send: "HERE ARE SOME THINGS YOU CAN DO"
+    stateStartingNow: "Default", // State between sender and respondHandler trigger delay
+    stateTransitioner: null,
+    nextActions: null,
+  },
+  setReminder: {
+    entryStates: 'all', 
+    check: checkSetReminder, // Message NLP Processed is "Set reminder at time..."
+    stateStartingNow: "Default",
+    respondHandler: respondSetReminder, // Send: "REMINDER FOR 8:15AM SET"
+    stateTransitioner: null,
+    nextActions: null,
+  },
+  remind: {
+    noNextAction: 'no', // Once entered through STATE, skip respondHandler !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    entryStates: 'all',
+    check: checkSetReminder, // Is Poll and current time matches user's time
+    respondHandler: handleRemind, // Send: "TIME TO MEDITATE!"
+    stateStartingNow: "ReceivingReminder",
+    stateTransitioner: transitionRemind, // "Check if Done or Timeout" - selects next action based on nextActions and checks of those next actions
+    nextActions: [options, notDoneReason],
+  },
+  options: {
+    calledByAction: 'yes', // Trigger respondHandler
+    entryStates: 'ReceivingReminder',
+    check: checkOptions,
+    respondHandler: handleOptions, // "Virtue or Hindrance?"
+    stateStartingNow: "SelectingOptions",
+    stateTransitioner: transitionOptions,
+    nextActions: [virtues, hindrances] // "Check if Virtue or Hindrance"
+  },
+  notDoneReason: {
+    entryStates: 'ReceivingReminder',
+    sender: sendNotDoneReason, // "Why didn't you do it"
+    stateStartingNow: "WhyLazy",
+    respondHandler: handleNotDoneReason // "Log user reason"
+  }
+}
+
 const STATES = {
   Default: {
+    // potentialActions: help, setReminder, quitReminder, reminderSent...
+    // potentialActionsCheck: helpCheck, setReminderCheck, ...
+    // respHandlers: helpHandler, setReminderHandler...
 
   },
   ReminderSent: {
     description: "Wait for user to perform an action (postback) or for reminder to time out (poll)",
-    handler: handlerReminderSent,
-    outcomes: [
-      { waitState: "DoneOptions", type: "postback" },
-      { waitState: "NotDoneReason", type: "poll" }
+    handler: handlerReminderSent, // "trigger: {text: "meditation time", content: {quickReply}}"
+    nextStates: [
+      { waitState: "DoneOptions", type: "postback" }, // text: "Choose between virtue and hindrance"
+      { waitState: "NotDoneReason", type: "message" } // text: "Why didn't you do it"
     ],
     error: { handler: handlerError, waitState: "Default" }
   },
@@ -104,7 +193,7 @@ function setWaitState(userID, nextState) {
 }
 
 function evalNextState(userResp, currState) {
-  outcomes = STATES[currState]['outcomes'];
-  outcome = outcomes.find(obj => obj['waitState'] === userResp);
-  return outcome['waitState'];
+  nextStates = STATES[currState]['nextStates'];
+  nextState = nextStates.find(obj => obj['waitState'] === userResp);
+  return nextState['waitState'];
 }
