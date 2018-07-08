92d6774b6f0d6fb6de17cf6c20c9dfcc30ee3c08
Jason Yoo <jasonyoo0116@gmail.com>
2018-07-08 12:11:36 -0700

    updated update_code.sh

--- update_code.sh
+++ update_code.sh
@@ -6,27 +6,36 @@ BLACK=`tput sgr0`
 RED=`tput setaf 1`
 GREEN=`tput setaf 2`
 
-if [ -z "$VERSION" ]; then
-	echo "${RED}You must supply the version of p2e-bot and p2e-background.${BLACK}"
-	echo "Example Usage: VERSION='1.0.0' ./update_code.sh"
+if [ -z "$BOT" ] && [ -z "$BACKGROUND" ]; then
+	echo "${RED} You must supply the upgrade version for at least one of the services through environmental variable(s)."
+	echo "Example Usage: BOT='1.0.0' BACKGROUND='1.0.0' ./update_code.sh"
 	exit 1
 fi
 
 echo "Starting Update..."
 pushd ~/code
 
-curl -L "https://github.com/truebluejason/p2e-bot/archive/v${VERSION}.tar.gz" -o p2e-bot.tar.gz
-tar -zxf p2e-bot.tar.gz
-rm p2e-bot.tar.gz
-pushd "p2e-bot-${VERSION}"
-echo "Running 'npm install'..."
-npm install
-popd
-echo "${GREEN}p2e-bot updated.${BLACK}"
+if [ -z "$BOT" ]; then
+	VERSION="$BOT"
+	curl -L "https://github.com/truebluejason/p2e-bot/archive/v${VERSION}.tar.gz" -o p2e-bot.tar.gz
+	tar -zxf p2e-bot.tar.gz
+	rm p2e-bot.tar.gz
+	pushd "p2e-bot-${VERSION}"
+	echo "Running 'npm install'..."
+	npm install
+	popd
+	echo "${GREEN}p2e-bot updated.${BLACK}"
+fi
 
-curl -L "https://github.com/truebluejason/p2e-background/releases/download/v${VERSION}/p2e-background" -o p2e-background
-chmod +x p2e-background
-echo "${GREEN}p2e-background updated.${BLACK}"
+if [ -z "$BACKGROUND" ]; then
+	VERSION="$BACKGROUND"
+	if [ -f "p2e-background" ]; then
+		mv p2e-background p2e-background-old
+	fi
+	curl -L "https://github.com/truebluejason/p2e-background/releases/download/v${VERSION}/p2e-background" -o p2e-background
+	chmod +x p2e-background
+	echo "${GREEN}p2e-background updated.${BLACK}"
+fi
 
 popd
 echo "${GREEN}Done!${BLACK}"
