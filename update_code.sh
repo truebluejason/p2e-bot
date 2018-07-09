a2bd05a2f3bb5a619061d732c0989a3db6f963d2
Jason Yoo <jasonyoo0116@gmail.com>
2018-07-08 21:51:40 -0700

    deployment related changes

--- update_code.sh
+++ update_code.sh
@@ -7,35 +7,37 @@ RED=`tput setaf 1`
 GREEN=`tput setaf 2`
 
 if [ -z "$BOT" ] && [ -z "$BACKGROUND" ]; then
-	echo "${RED} You must supply the upgrade version for at least one of the services through environmental variable(s)."
+	echo "${RED} You must supply the upgrade version for at least one of the services through environmental variable(s).${BLACK}"
 	echo "Example Usage: BOT='1.0.0' BACKGROUND='1.0.0' ./update_code.sh"
 	exit 1
 fi
 
 echo "Starting Update..."
-pushd ~/code
+pushd ~/code 1>/dev/null
 
-if [ -z "$BOT" ]; then
+if [ -n "$BOT" ]; then
 	VERSION="$BOT"
-	curl -L "https://github.com/truebluejason/p2e-bot/archive/v${VERSION}.tar.gz" -o p2e-bot.tar.gz
+	echo "Downloading tar file..."
+	curl -L "https://github.com/truebluejason/p2e-bot/archive/v${VERSION}.tar.gz" -o p2e-bot.tar.gz 2>/dev/null
 	tar -zxf p2e-bot.tar.gz
 	rm p2e-bot.tar.gz
-	pushd "p2e-bot-${VERSION}"
+	pushd "p2e-bot-${VERSION}" 1>/dev/null
 	echo "Running 'npm install'..."
-	npm install
-	popd
+	npm install 1>/dev/null
+	popd 1>/dev/null
 	echo "${GREEN}p2e-bot updated.${BLACK}"
 fi
 
-if [ -z "$BACKGROUND" ]; then
+if [ -n "$BACKGROUND" ]; then
 	VERSION="$BACKGROUND"
+	echo "Downloading binary..."
 	if [ -f "p2e-background" ]; then
 		mv p2e-background p2e-background-old
 	fi
-	curl -L "https://github.com/truebluejason/p2e-background/releases/download/v${VERSION}/p2e-background" -o p2e-background
+	curl -L "https://github.com/truebluejason/p2e-background/releases/download/v${VERSION}/p2e-background" -o p2e-background 2>/dev/null
 	chmod +x p2e-background
 	echo "${GREEN}p2e-background updated.${BLACK}"
 fi
 
-popd
+popd 1>/dev/null
 echo "${GREEN}Done!${BLACK}"
